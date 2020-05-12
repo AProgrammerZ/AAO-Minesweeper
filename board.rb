39c5716b9a896c78d54046b94643b479fea1cbd1
Aharon <aharonzarnighian@gmail.com>
2020-05-12 14:52:49 -0400

    Added seed_bombs method

--- board.rb
+++ board.rb
@@ -1,5 +1,19 @@
+require "byebug"
+
 class Board
     def initialize
-        @board = Array.new(9) {Array.new(9)}
+        @board = Array.new(9) {Array.new(9,"*")}
+    end
+
+    def seed_bombs
+        bomb_count = 0
+        until bomb_count == 10 do 
+            random_row_num = rand(0...@board.length)
+            random_tile_num = rand(0...@board.length)
+            unless @board[random_row_num][random_tile_num] == "B"
+                @board[random_row_num][random_tile_num] = "B" 
+                bomb_count += 1
+            end
+        end
     end
 end
