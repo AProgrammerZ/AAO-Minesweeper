6b8a1f0b454fbd9f56916816b932277c5d048b61
Aharon <aharonzarnighian@gmail.com>
2020-05-13 11:52:33 -0400

    Added fill_with_tiles method to Board

--- board.rb
+++ board.rb
@@ -4,9 +4,14 @@ require_relative "./tile.rb"
 class Board
     def initialize
         @board = Array.new(9) {Array.new(9)}
+        self.fill_with_tiles
         self.seed_bombs
     end
 
+    def fill_with_tiles
+        @board.map! { |row| row.map! { |square| Tile.new } }            
+    end
+    
     def seed_bombs
         bomb_count = 0
         until bomb_count == 10 do 
